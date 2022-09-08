9834f3c5842b34c69f925caa04d4a86f48be657c
angelicapedroso <angelicapedroso05@gmail.com>
2022-09-08 16:14:18 -0300

    desafio 5

---
+++ challenges/desafio5.js
@@ -0,0 +1,8 @@
+db.produtos
+  .find({
+    $or: [
+      { curtidas: 36 },
+      { vendidos: 85 },
+    ],
+  }, { _id: false, nome: true, vendidos: true, curtidas: true })
+  .sort({ vendidos: -1 });
